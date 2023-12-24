be94754744c11bf5915e393b6b1caa23b77664b2
frank <kuete.frank1@gmail.com>
2023-12-24 05:34:39 +0100

    v1-expense-tracker

--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,13 @@
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 db = SQLAlchemy(app)
